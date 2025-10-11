d862e7d109e03689fa65bdbf60e83e56516c768b
Roderick Demarais <RoderickDemarais@aol.com>
2025-10-10 23:31:41 -0500

    ordering: runtime-first Supabase client + /public/env.js (OT project)

--- public/env.js
+++ public/env.js
@@ -6,14 +6,13 @@
   const ENV = Object.freeze({
     VITE_SUPABASE_URL: "https://hpgjbpvugasktphwntee.supabase.co",
     VITE_SUPABASE_ANON_KEY:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwZ2picHZ1Z2Fza3RwaHdudGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNjQ2MjYsImV4cCI6MjA3NDg0MDYyNn0.rdBeHuUhHa_4yrdzXHnGzcXAolrcfPKV2mQhdjcgnYY",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwZ2picHZ1Z2Fza3RwaHdudGVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkyNjQ2MjYsImV4cCI6MjA3NDg0MDYyNn0.rdBeHuUhHa_4yrdzXHnGzcXAolrcfPKV2mQhdjcgnYY"
   });
 
-  // Don’t allow later scripts to mutate these values.
   Object.defineProperty(window, "__PUBLIC_ENV__", {
     value: ENV,
     writable: false,
     configurable: false,
-    enumerable: false,
+    enumerable: false
   });
 })();
